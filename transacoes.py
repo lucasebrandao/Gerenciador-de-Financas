33dddba27e7ba602c03ae73115a178b77b5a4209
Lucas Brandão <lucasebrandao3@gmail.com>
2026-08-05 12:57:53 -0300

    feat: adiciona tipos de transação (receita e despesa)

--- transacoes.py
+++ transacoes.py
@@ -17,9 +17,22 @@ def adicionar_transacao(transacoes):
     descricao = input("Insira uma descrição da transação: ")
     valor = float(input("Diga o valor da transação: "))
 
+    print("\nSelecione o tipo de transação: ")
+    print("1 - Receita")
+    print("2 - Despesa")
+
+    opcao = input("\nOpção: ")
+
+    if opcao == "1":
+        tipo = 'Receita'
+
+    if opcao == "2":
+        tipo = 'Despesa'
+    
     transacao = {
         'descricao': descricao,
-        'valor': valor
+        'valor': valor,
+        'tipo' : tipo
     }
 
     transacoes.append(transacao)
@@ -29,7 +42,7 @@ def listar_transacoes(transacoes):
 
     for transacao in transacoes:
 
-        print(f"{transacao['descricao']} - R${transacao['valor']:.2f}")
+        print(f"{transacao['descricao']} - R${transacao['valor']:.2f} - {transacao['tipo']}")
 
     if not transacoes:
         print("Sem transações registradas.")
@@ -40,7 +53,12 @@ def ver_saldo(transacoes):
     saldo = 0
 
     for transacao in transacoes:
-        saldo += transacao['valor']
+
+        if transacao['tipo'] == 'Receita':
+            saldo += transacao['valor']
+
+        if transacao['tipo'] == 'Despesa':   
+            saldo -= transacao['valor']
 
     print(f"R${saldo:.2f}")
 
